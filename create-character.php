c001a442ba3e8be9e2c7deca6dea70277af97071
Dedalus <dias.julien945@gmail.com>
2020-11-03 12:03:18 +0100

    add css for template

--- create-character.php
+++ create-character.php
@@ -82,200 +82,196 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 
 ?>
-
 <!DOCTYPE html>
-
 <html lang='fr'>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Create Character</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="asset/style.scss">
     </head>
     <body>
     <div class="container">
+     
         <br><br>
         <h1>INFORMATIONS PRINCIPALES</h1>
         <br><br>
         <form action="" method="post">
-           
-           <div class="col">
+        <div class="col">
+           <div class="row">
                <label for="nom">NOM: </label>
                <input class="form-control form-control-lg" type="text" placeholder="NOM" name="nom">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="prenom">PRENOM: </label>
                <input class="form-control form-control-lg" type="text" placeholder="PRENOM" name="prenom" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="age">AGE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="AGE" name="age">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="race">RACE: </label>
                <input class="form-control form-control-lg" type="text" placeholder="RACE" name="race" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="sexe">SEXE: </label>
                <input class="form-control form-control-lg" type="text" placeholder="SEXE" name="sexe">
            </div>  
-           <div class="col">
+           <div class="row">
                <label for="taille">TAILLE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="Entrez la taille en CM" name="taille">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="poids">POIDS: </label>
                <input class="form-control form-control-lg" type="number" placeholder="POIDS EN KILOS" name="poids">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="yeux">YEUX: </label>
                <input class="form-control form-control-lg" type="text" placeholder="YEUX" name="yeux">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="cheveux">CHEVEUX: </label>
                <input class="form-control form-control-lg" type="text" placeholder="CHEVEUX" name="cheveux">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="rang">RANG: </label>
                <input class="form-control form-control-lg" type="text" placeholder="RANG" name="rang">
            </div>
        <br><br>
    <h1>CARACTERISTIQUES PHYSIQUES</h1>
    <br><br>
-           <div class="col">
+           <div class="row">
                <label for="vie">VIE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="VIE" name="vie" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="force">FORCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="FORCE" name="force" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mana">MANA: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MANA" name="mana">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="puissance">PUISSANCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="PUISSANCE" name="puissance">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="precis">PRECISION: </label>
                <input class="form-control form-control-lg" type="number" placeholder="PRECISION" name="precis" required>
            </div>
      
-           <div class="col">
+           <div class="row">
                <label for="initiative">INITIATIVE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="INITIATIVE" name="initiative" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="agilite">AGILITE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="AGILITE" name="agilite" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mental">MENTAL: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MENTAL" name="mental" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="intelligence">INTELLIGENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="INTELLIGENCE" name="intelligence" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="volonte">VOLONTE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="VOLONTE" name="volonte" required>
            </div>
    
-           <div class="col">
+           <div class="row">
                <label for="perception">PERCEPTION: </label>
                <input class="form-control form-control-lg" type="number" placeholder="PERCEPTION" name="perception" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="maitrise">MAITRISE GENERALE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAITRISE GENERALE" name="maitrise" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="parade">PARADE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="PARADE" name="parade">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="esquive">ESQUIVE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="ESQUIVE" name="esquive" required>
            </div>
-           <div class="col">
+           <div class="row">
                <label for="bloque">BLOQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="BLOQUE" name="bloque">
            </div> 
-   <div class="col">
+   <div class="row">
                <label for="encaissement">ENCAISSEMENT: </label>
                <input class="form-control form-control-lg" type="number" placeholder="ENCAISSEMENT" name="encaissement">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="resilience">RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="RESILIENCE" name="resilience">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="critique">CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="CRITIQUE" name="critique">
            </div>
    <br><br>
    <h1>CARACTERISTIQUES MAGIQUES</h1>
    <br><br>
-           <div class="col">
+           <div class="row">
                <label for="mrr">MAGIE ROUGE RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE ROUGE RESILIENCE" name="magRedRes">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mbr">MAGIE BLEUE RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE BLEUE RESILIENCE" name="magBlueRes">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mbr1">MAGIE BLANCHE RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE BLANCHE RESILIENCE" name="magWhiteRes">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mvr">MAGIE VERTE RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE VERTE RESILIENCE" name="magGreenRes">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mnr">MAGIE NOIRE RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE NOIRE RESILIENCE" name="magBlackRes">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mpr">MAGIE PRISMA RESILIENCE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE PRISMA RESILIENCE" name="magPrismRes">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mrc">MAGIE ROUGE CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE ROUGE CRITIQUE" name="magRedCrit">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mbc">MAGIE BLEUE CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE BLEUE CRITIQUE" name="magBlueCrit">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mbc1">MAGIE BLANCHE CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE BLANCHE CRITIQUE" name="magWhiteCrit">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mvc">MAGIE VERTE CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE VERTE CRITIQUE" name="magGreenCrit">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mnc">MAGIE NOIRE CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE NOIRE CRITIQUE" name="magBlackCrit">
            </div>
-           <div class="col">
+           <div class="row">
                <label for="mpc">MAGIE PRISMA CRITIQUE: </label>
                <input class="form-control form-control-lg" type="number" placeholder="MAGIE PRISMA CRITIQUE" name="magPrismCrit">
            </div>
            <br>
            <input class='btn btn-primary' type='submit' name='valider' value='valider'>
-     
+           </div>
    </form>
-
-   </div>
-         <?php
-     
-       ?>
+</div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
--- create-user.php
+++ create-user.php
@@ -69,7 +69,7 @@ if(isset($_POST['valider'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Inscription</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="asset/style.css">
+        <link rel="stylesheet" href="asset/style.scss">
     </head>
     <body>
     <div class="container">
